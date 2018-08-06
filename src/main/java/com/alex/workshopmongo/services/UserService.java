c292147c48c87494abe7f1c64f8967bad7dcffb3
netoap <alexanderneto5@gmail.com>
2018-08-06 14:20:32 +0100

    Deleção de usuario com DELETE

--- src/main/java/com/alex/workshopmongo/services/UserService.java
+++ src/main/java/com/alex/workshopmongo/services/UserService.java
@@ -26,10 +26,13 @@ public class UserService {
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 	
-	public User insert(User obj) {
-		
-		return repo.insert(obj);
-		
+	public User insert(User obj) {		
+		return repo.insert(obj);		
+	}
+	
+	public void delete(String id) {
+		findById(id);
+		repo.deleteById(id);
 	}
 	
 	public User fromDTO (UserDTO objDto) {
